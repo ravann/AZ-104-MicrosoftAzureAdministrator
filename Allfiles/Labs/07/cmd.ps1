4c1272d98d284f578d5bc8254b719cb19dbaea63
Ravan <ravan.nannapaneni@oliverwyman.com>
2021-08-08 17:01:02 +0800

    Added removing and creating resource group ...

--- Allfiles/Labs/07/cmd.ps1
+++ Allfiles/Labs/07/cmd.ps1
@@ -1,3 +1,8 @@
+$location = 'southeastasia'
+$rgName = 'az104-07-rg0'
+
+New-AzResourceGroup -Name $rgName -Location $location
+
 New-AzResourceGroupDeployment `
    -ResourceGroupName $rgName `
    -TemplateFile $HOME/07/az104-07-vm-template.json `
