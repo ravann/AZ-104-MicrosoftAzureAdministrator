181b2f15526018b01660571bdae96339af05271b
Ravan <ravan.nannapaneni@oliverwyman.com>
2021-08-08 16:39:29 +0800

    Fixed the file names ...

--- Allfiles/Labs/07/cmd.ps1
+++ Allfiles/Labs/07/cmd.ps1
@@ -1,4 +1,4 @@
 New-AzResourceGroupDeployment `
    -ResourceGroupName $rgName `
-   -TemplateFile $HOME/07/az104-07-vms-template.json `
-   -TemplateParameterFile $HOME/07/az104-07-vms-parameters.json `
+   -TemplateFile $HOME/07/az104-07-vm-template.json `
+   -TemplateParameterFile $HOME/07/az104-07-vm-parameters.json `
